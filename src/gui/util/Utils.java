1c9e6b138a9a28ef2ce93f308dc445e7dabab4ac
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-04-05 18:23:16 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -10,4 +10,14 @@ public class Utils {
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
+	
+	// Converte o valor do TextField para int
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch (NumberFormatException e) {
+			return null;
+		}
+	}
 }
